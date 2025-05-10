d4f6d372c69bb4c3ba141a0cf05399e9c0bff1d8
danycosta40 <danyblandino@gmail.com>
2025-05-10 14:29:09 +0000

    refatorei o meu codigo para dividir responsabilidades

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from models.transaction import Transaction
+
+
 class Initialize():
-    def __init__(self):
-        self.__transactions = []
 
     def show_menu(self):
         print('\n')
@@ -26,13 +27,11 @@ class Initialize():
         value = input('Informe o valor: ')
         description = input('Informe descrição: ')
 
-        self.__transactions.append(
-            (operation, value, description)
-        )
+        transaction = Transaction(operation, value, description)
+        transaction.save()
 
     def to_view(self):
-        for transaction in self.__transactions:
-            print(f'Operation: {transaction[0]} - Value: {transaction[1]} - Description: {transaction[2]}')
+        Transaction().view()
 
     def to_go_out(self):
         print('\nObrigado, volte sempre!')
---
+++ utils/utils.py
@@ -0,0 +1,16 @@
+from configurations.configurations import Configurations
+from datetime import date
+
+class Utils():
+    def __init__(self):
+        self.__configurations = Configurations()
+
+    def read_file(self):
+        with open(self.__configurations.file_output, 'r') as file:
+            return list(map(lambda x: x.replace('/n', ''), file.readlines()))
+
+    def write_file(self, _type, value, description):
+        with open(self.__configurations.file_output, 'a+') as file:
+            file.write(f'{str(date.today())} - {_type} - R$ {value} - {description}')
+        
+
